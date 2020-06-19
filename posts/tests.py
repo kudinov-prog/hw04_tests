4e610be005a4122e6dfc5bf5a5f83b82e12e5b9e
Kudinov Mikhail <kudinov.mos@gmail.com>
2020-06-19 21:42:33 +0300

    Tests passed

--- posts/tests.py
+++ posts/tests.py
@@ -1,49 +1,66 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import Post, Group, User
 
 
 class TestPostsMethods(TestCase):
-    """
-    1.После регистрации пользователя создается его персональная 
-      страница (profile)
-    2.Авторизованный пользователь может опубликовать пост (new)
-    3.Неавторизованный посетитель не может опубликовать пост 
-      (его редиректит на страницу входа)
-    4.После публикации поста новая запись появляется на главной странице 
-      сайта (index), на персональной странице пользователя (profile), 
-      и на отдельной странице поста (post)
-    5.Авторизованный пользователь может отредактировать свой пост и 
-      его содержимое изменится на всех связанных страницах
-
-    """
 
     def setUp(self):
+        self.client = Client()
+
         self.test_text = 'old text'
         self.new_test_text = 'new text'
         self.test_message_id = '9999'
 
-        self.client = client()
+        self.new_user = User.objects.create_user(
+          username='test_user', password='12345'
+          )
 
-        self.new_user = User.objects.create_user(username='test_user', password='12345')
+        self.new_group = Group.objects.create(
+          title='test_group', slug='test_group'
+          )
 
-        self.new_group = Group.objects.create(title='test_group', slug='test_group')
+        self.new_post = Post.objects.create(
+          author=self.new_user,text=self.test_text,
+          group=self.new_group,id=self.test_message_id
+          )
 
-        self.new_post = Post.objects.create(author=self.new_user,
-                                            text=self.test_text = 'old text',
-                                            group=self.new_group,
-                                            id=self.test_message_id = '9999')
+        self.client.force_login(self.new_user)
 
-    def test_profile_reg(self):
-        pass
+    def test_create_profile_page(self):
+        """ После регистрации пользователя создается его персональная 
+            страница (profile)
+        """
+        response = self.client.get("/test_user/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["user"].username, self.new_user.username)
+
 
     def test_new_post(self):
-        pass
+        """Авторизованный пользователь может опубликовать пост (new)
+        """
+        data = {'text': self.test_text, "group": self.new_group}
+        response = self.client.post("/new/", data=data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
 
     def test_redirect(self):
-        pass
+        """Неавторизованный посетитель не может опубликовать пост 
+           (его редиректит на страницу входа)
+        """
+        self.client.logout()
+        data = {'text': self.test_text, "group": self.new_group}
+        response = self.client.post("/new/", data=data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
 
     def test_new_post_on_all_page(self):
+        """После публикации поста новая запись появляется на главной странице 
+           сайта (index), на персональной странице пользователя (profile), 
+           и на отдельной странице поста (post)"""
         pass
 
+
     def test_user_edit(self):
+        """Авторизованный пользователь может отредактировать свой пост и 
+           его содержимое изменится на всех связанных страницах"""
         pass
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Post, Group, User
 from .forms import PostForm
@@ -41,32 +41,35 @@ def new_post(request):
 
 def profile(request, username):
     user = get_object_or_404(User, username=username)
-    post_list = Post.objects.filter(author=user)
-    paginator = Paginator(post_list, 2)
+    posts = Post.objects.filter(author=user)
+    paginator = Paginator(posts, 2)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    return render(request, 'profile.html', {'page': page, 'paginator': paginator, 'user': user, 'author': user})
+    return render(request,
+                  'profile.html', {'page': page, 'paginator': paginator,
+                  'profile': user})
 
 
 def post_view(request, username, post_id):
     user = get_object_or_404(User, username=username)
-    post = Post.objects.filter(id=post_id)
-    post_single = Post.objects.filter(id=post_id)
-    post_list = Post.objects.filter(author=user)
-    return render(request, 'post.html', {'post_single': post_single, 'post_list': post_list, 'user': user, 'author': user, 'post': post})
+    post = get_object_or_404(Post, id=post_id)
+    count = Post.objects.filter(author=user).count()
+    return render(request, 'post.html', {'profile': user, 'post': post,
+                                         'count': count})
 
 
 @login_required
 def post_edit(request, username, post_id):
     post = get_object_or_404(Post, id=post_id)
     user = get_object_or_404(User, username=username)
+    post_url = reverse('post', args=(post.author, post_id))
     if user == request.user:
         form = PostForm(request.POST, instance=post)
         if request.method == 'POST' and form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post', username=post.author.username, post_id=post_id)
+            return redirect(post_url)
         form = PostForm(instance=post)
         return render(request, 'new.html', {'form': form, 'post': post})
-    return redirect('post', username=post.author.username, post_id=post_id)
+    return redirect(post_url)
