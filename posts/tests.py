41910de8e1475aea3fca72030ae0d6a9f1728ae8
Kudinov Mikhail <kudinov.mos@gmail.com>
2020-06-27 22:45:36 +0300

    All errors edits

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import redirect, reverse
 from django.test import TestCase, Client
 from .models import Post, Group, User
 
@@ -6,53 +6,36 @@ from .models import Post, Group, User
 class TestPostsMethods(TestCase):
 
     def setUp(self):
+
         self.client = Client()
+        self.user = User.objects.create_user(username='test_user',
+                                             password='12345')
+        self.group = Group.objects.create(title='group', slug='group')
+        self.client.force_login(self.user)
 
-        self.test_text = 'old text'
-        self.new_test_text = 'new text'
-        self.test_message_id = '1111'
-
-        self.new_user = User.objects.create_user(
-          username='test_user', password='12345'
-          )
-
-        self.test_group = Group.objects.create(
-          title='test_group', slug='test_group'
-          )
-        self.new_test_group = Group.objects.create(
-          title='new_test_group', slug='new_test_group'
-          )
-
-        self.new_post = Post.objects.create(
-          author=self.new_user, text=self.test_text,
-          group=self.test_group,id=self.test_message_id
-          )
-
-        self.client.force_login(self.new_user)
-
-    def test_create_profile_page(self):
+    def test_profile_page_exist(self):
         """ После регистрации пользователя создается его персональная 
             страница (profile)
         """
 
         response = self.client.get("/test_user/")
         self.assertEqual(response.context["user"].username,
-                         self.new_user.username)
+                         self.user.username)
+        self.assertEqual(response.status_code, 200)
 
     def test_new_post(self):
         """ Авторизованный пользователь может опубликовать пост (new)
         """
 
-        data = {'text': self.test_text, "group": self.test_group}
-        response = self.client.post(reverse("new_post"),
-                                    data=data, follow=True)
-
-        post = Post.objects.get(id=self.test_message_id)
-        check_list = (post.text, post.group, post.author)
+        data = {'text': 'text', 'group': self.group.id}
+        self.client.post(reverse("new_post"),
+                         data=data, follow=True)
+        post = Post.objects.first()
+        response = self.client.get(reverse('index'), follow=True)
+        check_list = (post.text, post.group.id, post.author)
 
         for item in check_list:
             self.assertContains(response, item)
-        
         self.assertEqual(response.status_code, 200)
         
     def test_unauthorized_user_post(self):
@@ -62,13 +45,12 @@ class TestPostsMethods(TestCase):
 
         self.client.logout()
 
-        data = {'text': self.test_text, "group": self.test_group}
+        data = {'text': 'text', "group": self.group.id}
         response = self.client.post(reverse("new_post"),
                                     data=data, follow=True)
-        
         posts_count = Post.objects.all().count()
 
-        self.assertEqual(posts_count, 1)
+        self.assertEqual(posts_count, 0)
         self.assertRedirects(response, '/auth/login/?next=/new/')
 
     def check_post(self, url, text, group, author):
@@ -91,39 +73,49 @@ class TestPostsMethods(TestCase):
             и на отдельной странице поста (post)
         """
 
+        new_post = Post.objects.create(author=self.user, text='text',
+                                       group=self.group, id='100')
         urls = (reverse('index'),
                 reverse('profile',
-                        kwargs={'username': self.new_user.username}),
+                        kwargs={'username': self.user.username}),
                 reverse('post',
-                        kwargs={'username': self.new_user.username,
-                                'post_id': self.test_message_id}))
+                        kwargs={'username': self.user.username,
+                                'post_id': new_post.id}))
 
         for url in urls:
-            self.check_post(url, self.new_post.text,
-                                 self.new_post.group,
-                                 self.new_post.author)
+            self.check_post(
+                url,
+                new_post.text,
+                new_post.group,
+                new_post.author
+            )
 
     def test_user_edit(self):
         """ Авторизованный пользователь может отредактировать свой пост и 
             его содержимое изменится на всех связанных страницах
         """
 
+        new_post = Post.objects.create(author=self.user, text='text',
+                                       group=self.group, id='100')
+        new_group = Group.objects.create(title='new_group', slug='new_group')
+
         self.client.post(reverse('post_edit',
-            kwargs={'username': self.new_user.username,
-                    'post_id': self.new_post.id}),
-            data={'text': self.new_test_text, 'group': self.new_test_group},
-            follow=True)
+                                 kwargs={'username': self.user.username,
+                                         'post_id': new_post.id}),
+                                 data={'text': 'new_text',
+                                       'group': new_group.id},
+                                 follow=True)
 
         urls = (reverse('index'),
                 reverse('group_posts',
-                        kwargs={'slug': self.test_group.slug}),
+                        kwargs={'slug': new_group.slug}),
                 reverse('profile',
-                        kwargs={'username': self.new_user.username}),
+                        kwargs={'username': self.user.username}),
                 reverse('post',
-                        kwargs={'username': self.new_user.username,
-                        'post_id': self.new_post.id}))
+                        kwargs={'username': self.user.username,
+                                'post_id': new_post.id}))
         
         for url in urls:
-            self.check_post(url, self.new_post.text,
-                                 self.new_post.group,
-                                 self.new_post.author)
+            self.check_post(url, 'new_text',
+                                 new_group,
+                                 self.user)
